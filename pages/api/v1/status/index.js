c1c40c0bcb2997b4bd84678630570546c26e3acb
Danilo Vieira <danilorocha722@gmail.com>
2025-05-07 21:10:10 -0300

    Finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  return res.status(200).json({ teste: "Teste API STATUS" });
+  const databaseName = process.env.POSTGRES_DB;
+  const updatedAt = new Date().toISOString();
+  const resultVersion = await database.query("SHOW server_version");
+  const resultMaxConnections = await database.query("SHOW max_connections;");
+  const resultUsedConnections = await database.query({
+    text: "SELECT count(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  return res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: resultVersion.rows[0].server_version,
+        max_connections: parseInt(resultMaxConnections.rows[0].max_connections),
+        opened_connections: parseInt(resultUsedConnections.rows[0].count),
+      },
+    },
+  });
 }
 
 export default status;
