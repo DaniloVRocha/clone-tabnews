30ec12d9dec658a993f16f256f5c28776b5a8906
DaniloVRocha <danilorocha722@gmail.com>
2025-08-26 23:51:02 -0300

    feat: cria o model de usuarios e o endpoint `api/v1/users` POST

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,5 @@
 import orchestrator from "tests/orchestrator.js";
-import database from "infra/database";
+import { version as uuidVersion } from "uuid";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -10,17 +10,108 @@ beforeAll(async () => {
 describe("POST /api/v1/users", () => {
   describe("Anonymous User", () => {
     test("with unique and valid data", async () => {
-      await database.query({
-        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
-        values: ["DaniloVRocha", "danilo@gmail.com", "senha123"],
-      });
-      const users = await database.query("select * from users");
-      console.log(users.rows);
-
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "DaniloVRocha",
+          email: "danilo@gmail.com",
+          password: "senha123",
+        }),
       });
+
       expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "DaniloVRocha",
+        email: "danilo@gmail.com",
+        password: "senha123",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at,
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    });
+    test("with duplicated 'email'", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "emailDuplicado",
+          email: "duplicado@gmail.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "emailDuplicado2",
+          email: "Duplicado@gmail.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(response2.status).toBe(400);
+
+      const responseBody2 = await response2.json();
+      expect(responseBody2).toEqual({
+        name: "ValidationError",
+        message: "Email já está em uso",
+        action: "Utilize outro email",
+        status_code: 400,
+      });
+    });
+
+    test("with duplicated 'username'", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "usernameDuplicado",
+          email: "usernameduplicado@gmail.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "usernameDuplicado",
+          email: "usernameduplicado2@gmail.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(response2.status).toBe(400);
+
+      const responseBody2 = await response2.json();
+      expect(responseBody2).toEqual({
+        name: "ValidationError",
+        message: "Username já está em uso",
+        action: "Utilize outro username",
+        status_code: 400,
+      });
     });
   });
 });
