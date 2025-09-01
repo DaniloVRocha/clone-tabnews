bdb7f4c39080a1983cc73f4c14b50e2d02e78fab
DaniloVRocha <danilorocha722@gmail.com>
2025-09-01 15:58:40 -0300

    feat: implementa rota de username especifico

---
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -0,0 +1,33 @@
+import orchestrator from "tests/orchestrator.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/users/[username]", () => {
+  describe("Anonymous User", () => {
+    test("with exact case match for username'", async () => {
+      const response1 = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "MesmoCase",
+          email: "mesmo.case@gmail.com",
+          password: "senha123",
+        }),
+      });
+
+      expect(response1.status).toBe(201);
+
+      const response2 = await fetch(
+        "http://localhost:3000/api/v1/users/MesmoCase",
+      );
+
+      expect(response2.status).toBe(200);
+    });
+  });
+});
